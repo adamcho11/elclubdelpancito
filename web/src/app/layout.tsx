59db3f8041cb660adda8ecccb9f3f4b0cd41416e
Adam Leanos <adamleanos@users.noreply.github.com>
2026-06-19 10:18:22 -0400

    fix: tawk.to no auto-abre + sugerencias contextuales

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -106,7 +106,28 @@ export default function RootLayout({
             id="tawk-to"
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{
-              __html: `var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();(function(){var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];s1.async=true;s1.src='https://embed.tawk.to/6a34846316fcef1d436fb118/1jreia9ri';s1.charset='UTF-8';s1.setAttribute('crossorigin','*');s0.parentNode.insertBefore(s1,s0);})();`,
+              __html: `var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+Tawk_API.onLoad = function(){
+  Tawk_API.hideWidget();
+  setTimeout(function(){ Tawk_API.showWidget(); }, 300);
+  Tawk_API.setAttributes({
+    proyecto: 'El Club del Pancito',
+    pagina: window.location.pathname
+  }, function(error){});
+};
+
+Tawk_API.onChatStarted = function(){
+  Tawk_API.addTags(["pan-artesanal","suscripcion","Sucre"]);
+};
+
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6a34846316fcef1d436fb118/1jreia9ri';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();`,
             }}
           />
           <StructuredData />
